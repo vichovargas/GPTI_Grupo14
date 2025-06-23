ada7b98a8be5a58a1bcb2442a45142f09784fd3f
wintermelyn <matas.seplveda@uc.cl>
2025-06-23 16:26:26 -0400

    Refactor availability and task models for improved readability and type safety

--- src/organizai/models/availability.py
+++ src/organizai/models/availability.py
@@ -2,13 +2,17 @@ from pydantic import BaseModel
 from typing import List, Literal
 from datetime import time
 
+
 class TimeRange(BaseModel):
     start_time: time
     end_time: time
 
+
 class DailyAvailability(BaseModel):
-    day: Literal['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
+    day: Literal['Lunes', 'Martes', 'Miercoles',
+                 'Jueves', 'Viernes', 'Sabado', 'Domingo']
     time_ranges: List[TimeRange]
+
 
 class Availability(BaseModel):
     days: List[DailyAvailability]
